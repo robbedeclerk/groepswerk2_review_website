2b5893813812f1b9bee54d7a1989f407c2478488
JorenVdG00 <jorenvdg1@gmail.com>
2024-02-18 23:24:55 +0100

    Caching gefixt

--- api/tmdb_api.py
+++ api/tmdb_api.py
@@ -1,15 +1,13 @@
-from abc import ABC, abstractmethod
+from cachetools import TTLCache, cached
 import requests
-import redis
 import json
-
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
 class Tmdb:
 
     def __init__(self, isMovie):
         self.isMovie = isMovie
+        self.cache = TTLCache(maxsize=1000, ttl=3600)
         self.headers = {
             "accept": "application/json",
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMGU4ZGIyOTNmZGRlOTc3YWEzMDQ0YjUwMmRmMDI5NyIsInN1YiI6IjY1YzIyOTdkZWZlYTdhMDE4NDUyY2I2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.r1F8ZMiAf1b20cYYmqtAuv7kCNBBHTOvAETk5kxI61I"
@@ -37,6 +35,7 @@ class Tmdb:
         else:
             print("Kon niet verbinden met TheMovieDB")
 
+    @cached(cache={})
     def list_of_genres(self):
         url = f"https://api.themoviedb.org/3/genre/{self.is_movie_arg()}/list?language=en"
         response = requests.get(url, headers=self.headers)
@@ -51,6 +50,7 @@ class Tmdb:
                 return genre['id']
         return None
 
+    @cached(cache={})
     def get_trending_data(self):
         url = f"https://api.themoviedb.org/3/trending/{self.is_movie_arg()}/day?language=en-US"
 
@@ -59,6 +59,7 @@ class Tmdb:
             data = response.json()
             return data
 
+    @cached(cache={})
     def get_top_rated_data(self):
         url = f"https://api.themoviedb.org/3/{self.is_movie_arg()}/top_rated?language=en-US&page=1"
 
@@ -67,6 +68,7 @@ class Tmdb:
             data = response.json()
             return data
 
+    @cached(cache={})
     def get_popular_data(self):
         url = f"https://api.themoviedb.org/3/{self.is_movie_arg()}/popular?language=en-US&page=1"
 
@@ -88,6 +90,7 @@ class Tmdb:
             movie_list.append(each[self.title_arg()])
         return movie_list
 
+    @cached(cache={})
     def get_data_filtered_genres_on_popularity(self, genre):
         genre_id = self.get_genre_id(genre)
         url = f"https://api.themoviedb.org/3/discover/{self.is_movie_arg()}?sort_by=popularity.desc&with_genres={genre_id}"  # 28=Action 35=Comedy
@@ -97,6 +100,7 @@ class Tmdb:
             data = response.json()
             return data
 
+    @cached(cache={})
     def get_poster(self, id):
         url = f"https://api.themoviedb.org/3/{self.is_movie_arg()}/{id}?language=en-US"
 
@@ -108,6 +112,7 @@ class Tmdb:
             poster_url = poster_base_url + poster_path
             return poster_url
 
+    @cached(cache={})
     def get_id(self, title):
         url = f"https://api.themoviedb.org/3/search/{self.is_movie_arg()}?query={title}&include_adult=true&language=en-US&page=1"
 
@@ -116,6 +121,7 @@ class Tmdb:
             data = response.json()
             return data["results"][0]["id"]
 
+    @cached(cache={})
     def get_data(self, id):
         url = f"https://api.themoviedb.org/3/{self.is_movie_arg()}/{id}?language=en-US"
 
@@ -124,43 +130,40 @@ class Tmdb:
             data = response.json()
             return data
 
+    @cached(cache={})
     def get_details(self, id):
-        cached_data = redis_client.get(f'{self.isMovie}_{id}')
-        if cached_data:
-            return json.loads(cached_data)
-        else:
-            url = f"https://api.themoviedb.org/3/{self.is_movie_arg()}/{id}?language=en-US"
+        print("Fetching data...")
+        url = f"https://api.themoviedb.org/3/{self.is_movie_arg()}/{id}?language=en-US"
 
-            response = requests.get(url, headers=self.headers)
-            if response.status_code == 200:
-                data = response.json()
-                poster_url = self.get_poster(id)
-                genres = data['genres']
-                genre = []
-                for each in genres:
-                    genre.append(each['name'])
-                if self.isMovie:
-                    movie_info = {
-                        "Title": data['title'],
-                        "Released": data['release_date'],
-                        "Runtime": data['runtime'],
-                        "Genre": genre,
-                        "Overview": data['overview'],
-                        "Poster": poster_url
-                    }
-                else:
-                    movie_info = {
-                        "Title": data['name'],
-                        "Released": data['first_air_date'],
-                        "Genre": genre,
-                        "Seasons": data['number_of_seasons'],
-                        "Episodes": data['number_of_episodes'],
-                        "Overview": data['overview'],
-                        "Poster": poster_url
-                    }
-                redis_client.set(f'{self.isMovie}_{id}', json.dumps(movie_info))
-                redis_client.expire(f'{self.isMovie}_{id}', 3600)
-                return movie_info
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            print("Data fetched from API.")
+            data = response.json()
+            poster_url = self.get_poster(id)
+            genres = data['genres']
+            genre = []
+            for each in genres:
+                genre.append(each['name'])
+            if self.isMovie:
+                movie_info = {
+                    "Title": data['title'],
+                    "Released": data['release_date'],
+                    "Runtime": data['runtime'],
+                    "Genre": genre,
+                    "Overview": data['overview'],
+                    "Poster": poster_url
+                }
+            else:
+                movie_info = {
+                    "Title": data['name'],
+                    "Released": data['first_air_date'],
+                    "Genre": genre,
+                    "Seasons": data['number_of_seasons'],
+                    "Episodes": data['number_of_episodes'],
+                    "Overview": data['overview'],
+                    "Poster": poster_url
+                }
+            return movie_info
 
     def get_list_details(self, data_list):
         list_details = []
@@ -185,14 +188,28 @@ class Tmdb:
         return list_details
 
 
+import time
+
 movie = Tmdb(True)
 serie = Tmdb(False)
 
+start_time = time.time()
 popular_details_data = movie.get_popular_details()
-movie.print_titles(movie.get_popular_data())
-print()
-
-serie.print_titles(serie.get_popular_data())
+end_time = time.time()
+exec_time_1 = end_time - start_time
+# movie.print_titles(movie.get_popular_data())
+# print()
+#
+# serie.print_titles(serie.get_popular_data())
 
 for thing in popular_details_data:
     print(thing)
+
+start_time = time.time()
+popular_details_data2 = movie.get_popular_details()
+end_time = time.time()
+exec_time_2 = end_time - start_time
+print(popular_details_data2[0])
+# Cijfers na ::: is alles cached
+print(f"ZonderCache: {exec_time_1}")  # AVG: 25.3, 25.1, 26.04:::25.4, 25.5
+print(f"MetCaching: {exec_time_2}")  # AVG: 8.57, 8.73, 8.7:::0, 0   TOP
