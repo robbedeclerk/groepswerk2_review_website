07931e3abd892221fcaba486b321eb14f0df1ddb
inias <iniasderoeck@gmail.com>
2024-02-26 11:51:53 +0100

    database verder uitgewerkt

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@<172.17.0.2>/moviedbContainer'
