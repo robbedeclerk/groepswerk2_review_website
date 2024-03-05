f392127cae223af51fc7986f5c31e210c06c0ba6
robbedeclerck <robbedc@hotmail.com>
2024-03-05 11:56:04 +0100

    init bloud verwijdert

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-<<<<<<< HEAD
-
-=======
->>>>>>> 0c757e84895f0cdd8a977791bc14ed4632ea9438
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-<<<<<<< HEAD
+
 #login = LoginManager(app)
-=======
-# login = LoginManager(app)
->>>>>>> 0c757e84895f0cdd8a977791bc14ed4632ea9438
 
 from app import routes, models, tmdb_api
