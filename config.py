3f76e712d1e9845b85e5f27bec23ba8d242dcb32
inias <iniasderoeck@gmail.com>
2024-03-10 11:10:00 +0100

    config file aangepast zodat enviorement variables kan gebruiken

--- config.py
+++ config.py
@@ -4,7 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/moviedb'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URI') or (f"postgresql://"
+                                                           f"{os.environ.get('DB_USER')}:"
+                                                           f"{os.environ.get('DB_USER_PASSWORD')}"
+                                                           f"@{os.environ.get('localhost')}"
+                                                           f"/{os.environ.get('DB_NAME')}"
+                                                           )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
