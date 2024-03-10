8bcfc15e5edff6496fa06fb6abe86eae49e9dd58
inias <iniasderoeck@gmail.com>
2024-03-10 14:01:02 +0100

    string bij secret key om makkelijk te werken

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URI') or (f"postgresql://"
                                                            f"{os.environ.get('DB_USER')}:"
                                                            f"{os.environ.get('DB_USER_PASSWORD')}"
