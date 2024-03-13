d4fc4a6b7ab89379e28abc6c98b354e58c1fb31a
inias <iniasderoeck@gmail.com>
2024-03-13 14:26:18 +0100

    login.html verbeterd en reset_password.html veranderd en de new gedelete

--- config.py
+++ config.py
@@ -14,4 +14,8 @@ class Config:
                                                            f"/{os.environ.get('DB_NAME')}"
                                                            )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    # MAIL_SERVER = 'smtp.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
