065f2375e3a50c158fe49f81b23334e90001b340
inias <iniasderoeck@gmail.com>
2024-03-13 16:18:07 +0100

    vanalles

--- app/usermail.py
+++ app/usermail.py
@@ -31,6 +31,6 @@ def send_password_reset_email(user):
         "[Groepswerk]Password Reset",
         sender=current_app.config["ADMINS"][0],
         recipients=[user.email],
-        text_body=render_template("reset_password_new.txt", user=user, token=token),
-        html_body=render_template("reset_password_new.html", user=user, token=token),
+        text_body=render_template("email/reset_password_new.txt", user=user, token=token),
+        html_body=render_template("email/reset_password_request.html", user=user, token=token),
     )
